6d9c1b5def61455d0f7da1857283cacd386d911d
Colin Perepelken <colin@lingellearning.com>
2021-06-09 21:14:05 -0700

    Add language strings to form

--- classes/form/assign_form.php
+++ classes/form/assign_form.php
@@ -15,20 +15,19 @@ class assign_form extends moodleform {
         $mform =& $this->_form;
 
         // TODO add description of tool function
-        // TODO use language strings below
 
         // Text in title
-        $mform->addElement('text', 'title', 'Text in title');
+        $mform->addElement('text', 'title', get_string('titlematch', 'tool_bulk_rubric_assign'));
         $mform->setType('title',PARAM_TEXT);
         $mform->addRule('title', 'required', 'required');
 
         // Course select
-        $mform->addElement('select', 'course', 'Course', [1 => 'Course 1', 2 => 'Course 2']);
+        $mform->addElement('select', 'course', get_string('course'), [1 => 'Course 1', 2 => 'Course 2']);
         $mform->setType('course', PARAM_INT);
         $mform->addRule('course', 'required', 'required');
 
         // Rubric template select
-        $mform->addElement('select', 'template', 'Rubric', [1 => 'Example template']);
+        $mform->addElement('select', 'template', get_string('rubric', 'tool_bulk_rubric_assign'), [1 => 'Example template']);
         $mform->setType('template', PARAM_INT);
         $mform->addRule('template', 'required', 'required');
 
--- lang/en/tool_bulk_rubric_assign.php
+++ lang/en/tool_bulk_rubric_assign.php
@@ -5,4 +5,5 @@ defined('MOODLE_INTERNAL') || die;
 $string['pluginname'] = 'Bulk rubric assignment';
 $string['title']      = 'Bulk rubric assignment';
 $string['heading']    = 'Bulk rubric assignment';
-
+$string['titlematch'] = 'Text in title';
+$string['rubric']     = 'Rubric template';
